00b17a444ddc7f5df3a73b42ffe0500adbd33101
snghnishant <blackmalware@protonmail.com>
2020-05-19 20:27:04 +0530

    added transition to main content

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const toggler = document.getElementById('toggler');
 const nav = document.getElementById('menu');
 const main = document.getElementById('main');
-const menuItem = document.getElementById('menu-items');
+
 function menu(){
     if(toggler.checked){
         main.style.marginLeft = "250px";
         nav.style.width = "250px";
         main.style.opacity = "0.3";
+        main.style.paddingLeft = "30px";
     }else{
         main.style.marginLeft = "0";
         nav.style.width = "0";
         main.style.opacity = "1";
+        main.style.paddingLeft = "0";
     }
 }
