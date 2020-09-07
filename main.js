a7f9928df09f1a6060d41e81d5498772ef1e9bfc
snghnishant <blackmalware@protonmail.com>
2020-09-07 18:15:29 +0530

    updated footer, landing, and sidemenu

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ function menu(){
         main.style.marginLeft = "250px";
         nav.style.width = "250px";
         main.style.opacity = "0.3";
-        main.style.paddingLeft = "30px";
+        
     }else{
         main.style.marginLeft = "0";
         nav.style.width = "0";
         main.style.opacity = "1";
-        main.style.paddingLeft = "0";
+        
     }
 }
